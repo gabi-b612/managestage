1119b474408991129be095742eb54e87036e29a6
gabi-b612 <20km095@esisalama.org>
2024-09-13 09:20:22 +0200

    fixed frond-end Dashboar [left panel]

--- src/main/java/com/managestage/managestage/MainApplication.java
+++ src/main/java/com/managestage/managestage/MainApplication.java
@@ -15,26 +15,34 @@ public class MainApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
 
-        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("LoginController.fxml")));
-
-        AtomicReference<Double> xOffset = new AtomicReference<>((double) 0);
-        AtomicReference<Double> yOffset = new AtomicReference<>((double) 0);
-
-        root.setOnMousePressed(event -> {
-            xOffset.set(event.getSceneX());
-            yOffset.set(event.getSceneY());
-        });
-
-        root.setOnMouseDragged(event -> {
-            stage.setX(event.getScreenX() - xOffset.get());
-            stage.setY(event.getScreenY() - yOffset.get());
-        });
+        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("dashboard-admin.fxml")));
 
         Scene scene = new Scene(root);
 
-        stage.initStyle(StageStyle.UNDECORATED);
         stage.setScene(scene);
+        stage.setResizable(false);
         stage.show();
+
+//        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("LoginController.fxml")));
+//
+//        AtomicReference<Double> xOffset = new AtomicReference<>((double) 0);
+//        AtomicReference<Double> yOffset = new AtomicReference<>((double) 0);
+//
+//        root.setOnMousePressed(event -> {
+//            xOffset.set(event.getSceneX());
+//            yOffset.set(event.getSceneY());
+//        });
+//
+//        root.setOnMouseDragged(event -> {
+//            stage.setX(event.getScreenX() - xOffset.get());
+//            stage.setY(event.getScreenY() - yOffset.get());
+//        });
+//
+//        Scene scene = new Scene(root);
+//
+//        stage.initStyle(StageStyle.UNDECORATED);
+//        stage.setScene(scene);
+//        stage.show();
     }
 
     public static void main(String[] args) {
---
+++ src/main/java/com/managestage/managestage/controller/DashboardAdmin.java
@@ -0,0 +1,4 @@
+package com.managestage.managestage.controller;
+
+public class DashboardAdmin {
+}
